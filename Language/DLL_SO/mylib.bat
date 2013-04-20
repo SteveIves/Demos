e1bafc5d12c571fa820b919b372ec1e36bc0d617
Steve Ives <steve.ives@synergex.com>
2013-04-20 05:15:31 +0000

    Reviewed and updated
    git-svn-id: http://sivesw10:8080/svn/Dev@120 419f3c83-0261-5a45-95fb-b078c30c8bb1

--- Language/DLL_SO/mylib.bat
+++ Language/DLL_SO/mylib.bat
@@ -1,32 +1,22 @@
-@echo off
 REM ------------------------------------
 REM Script to build custom DLL mylib.dll
 REM ------------------------------------
 
 setlocal
 
-call "C:\Program Files\Microsoft Visual Studio 8\VC\vcvarsall.bat"
+call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat"
 
 cl -nologo -MD -Ox -DWIN32 -DWINDOWS -c -GF -Ob2 -DNDEBUG -I "%DBLDIR%\csrc" power.c
-
 lib -nologo -def:mylib.def -out:mylib.lib power.obj "%DBLDIR%\lib\xclapi.obj"
-
 link -nologo -opt:ref -dll -out:mylib.dll power.obj mylib.exp "%DBLDIR%\lib\xclapi.obj" "%DBLDIR%\lib\xclapif.obj"
 
 REM This command only required if using Visual Studio 2005
-mt -nologo -manifest mylib.dll.manifest  -outputresource:mylib.dll;2
+rem mt -nologo -manifest mylib.dll.manifest  -outputresource:mylib.dll;2
 
 endlocal
 
-dbl -dXT power.dbl
-dblink -d power
-
-del mylib.dll.manifest
-del mylib.exp
-del mylib.lib
-del power.obj
-del power.dbo
-
-dbs power
+if exist mylib.dll.manifest del mylib.dll.manifest
+if exist mylib.exp del mylib.exp
+if exist mylib.lib del mylib.lib
 
 pause
