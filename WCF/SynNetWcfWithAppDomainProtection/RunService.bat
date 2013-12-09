482faa83a2fae23b1894596f29cedefd61b24036
Steve Ives <steve.ives@synergex.com>
2013-12-09 23:07:09 +0000

    Reorganized
    git-svn-id: http://sivesw10:8080/svn/Dev@186 419f3c83-0261-5a45-95fb-b078c30c8bb1

--- WCF/SynNetWcfWithAppDomainProtection/RunService.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set DEVROOT=%~dp0
-cd /d %DEVROOT%
-set DAT=%DEVROOT%data
-ServiceHost\Bin\Debug\ServiceHost.exe
--- WCF/SynNetWcfWithAppDomainProtection/develop.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-set DEVROOT=%~dp0
-cd /d %DEVROOT%
-cd
-set RPSMFIL=%DEVROOT%rps\rpsmain.ism
-set RPSTFIL=%DEVROOT%rps\rpstext.ism
-set DAT=%DEVROOT%data
-start SynNetWcfWithAppDomainProtection.sln
-exit
