835003c907770cd3c030f35753507599819288d9
Steve Ives <steve.ives@synergex.com>
2014-03-07 19:03:08 +0000

    Clean up
    git-svn-id: http://sivesw10:8080/svn/Dev@234 419f3c83-0261-5a45-95fb-b078c30c8bb1

--- SQL/Replication/BAT/regen.bat
+++ SQL/Replication/BAT/regen.bat
@@ -1,2 +1,20 @@
-call codegen -s EMPLOYEE   -r -v -t synioreplicate sqlio
-call codegen -s DEPARTMENT -r -v -t synio
+@echo off
+setlocal
+
+rem Configure the CodeGen environment
+
+set ROOT_FOLDER=%~dp0..
+set CODEGEN_TPLDIR=%ROOT_FOLDER%\SRC\TEMPLATES
+set CODEGEN_OUTDIR=%ROOT_FOLDER%\SRC\LIBRARY
+set CODEGEN_AUTHOR="Steve Ives"
+set CODEGEN_COMPANY="Synergex Professional Services Group"
+
+rem In this example we are using CodeGen Structure Mapping, so the structures
+rem being used are structures that have been created for this purpose. They
+rem have different names and field names to the original structures, and
+rem may have various field data manipulations that take place.
+
+codegen -s EMPLOYEE   -t synioreplicate sqlio  -r -v
+codegen -s DEPARTMENT -t synio                 -r -v
+
+endlocal
