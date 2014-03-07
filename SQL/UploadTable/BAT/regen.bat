5aec50dba7e377a0e0e61a48e2cfa2713a8698fd
Steve Ives <steve.ives@synergex.com>
2014-03-07 18:36:30 +0000

    Cleaned up
    git-svn-id: http://sivesw10:8080/svn/Dev@230 419f3c83-0261-5a45-95fb-b078c30c8bb1

--- SQL/UploadTable/BAT/regen.bat
+++ SQL/UploadTable/BAT/regen.bat
@@ -1,8 +1,35 @@
 @echo off
-codegen -t DatabaseTableTest                    -n SqlUploadDemo    -v
-codegen -t DatabaseTableMapped -s CUSTOMER      -n SqlUploadDemo -r -v
-codegen -t DatabaseTableMapped -s ORDER_HEADER  -n SqlUploadDemo -r -v
-codegen -t DatabaseTableMapped -s ORDER_LINE    -n SqlUploadDemo -r -v
-codegen -t DatabaseTableMapped -s PRODUCT       -n SqlUploadDemo -r -v
-codegen -t DatabaseTableMapped -s PRODUCT_GROUP -n SqlUploadDemo -r -v
+setlocal
 
+rem Configure the CodeGen environment
+
+set ROOT_FOLDER=%~dp0..
+set CODEGEN_TPLDIR=%ROOT_FOLDER%\TEMPLATES
+set CODEGEN_OUTDIR=%ROOT_FOLDER%\SRC
+set CODEGEN_AUTHOR="Steve Ives"
+set CODEGEN_COMPANY="Synergex Professional Services Group"
+
+rem In this example we are using CodeGen Structure Mapping, so the structures
+rem being used are structures that have been created for this purpose. They
+rem have different names and field names to the original structures, and
+rem may have various field data manipulations that take place.
+
+rem Generate the test program
+codegen                  -t DatabaseTableTest   -n SqlUploadDemo -v
+
+rem Generate code for CUSTOMER (original structure CUSMAS)
+codegen -s CUSTOMER      -t DatabaseTableMapped -n SqlUploadDemo -v -r
+
+rem Generate code for PRODUCT_GROUP (original structure INVGRP)
+codegen -s PRODUCT_GROUP -t DatabaseTableMapped -n SqlUploadDemo -v -r
+
+rem Generate code for PRODUCT (original structure INVMAS)
+codegen -s PRODUCT       -t DatabaseTableMapped -n SqlUploadDemo -v -r
+
+rem Generate code for ORDER_HEADER (original structure ORDHDR)
+codegen -s ORDER_HEADER  -t DatabaseTableMapped -n SqlUploadDemo -v -r
+
+rem Generate code for ORDER_LINE (original structure ORDLIN)
+codegen -s ORDER_LINE    -t DatabaseTableMapped -n SqlUploadDemo -v -r
+
+endlocal
