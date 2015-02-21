80ccfe5659fd0776eec5bf83df8e44fd027dff9b
Steve Ives <steve.ives@synergex.com>
2015-02-21 03:24:09 +0000

    Added a program to show pending changes, but it crashes the compiler so the .DBL is not in the project right now.
    git-svn-id: http://sivesw10:8080/svn/Dev@455 419f3c83-0261-5a45-95fb-b078c30c8bb1

--- SQL/UploadNetChange/GenerateToolsCode.bat
+++ SQL/UploadNetChange/GenerateToolsCode.bat
@@ -5,7 +5,7 @@ pushd %~dp0
 set CODEGEN_TPLDIR=.\TEMPLATES
 set CODEGEN_OUTDIR=.\SRC\TOOLS
 
-codegen -e -s EMPLOYEE -t NetChangeInitialUpload NetChangeShowSnapshots NetChangeUploadChanges -ut DATABASE_NAME=SqlUploadNetChange -r -lf
+codegen -e -s EMPLOYEE -t NetChange* -ut DATABASE_NAME=SqlUploadNetChange -r -lf
 
 popd
 endlocal
